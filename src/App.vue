13cf8ba8d899922016f0e1b9ea66414b90708246
BokiGaja <bgajic93@gmail.com>
2019-01-05 14:45:40 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+    <!--<app-main></app-main>-->
+  </div>
+</template>
+
+<script>
+  import Header from "./components/Header.vue"
+  import Main from "./components/Main.vue"
+  export default {
+    components: {
+      appHeader: Header,
+      appMain: Main
+    }
+  }
+</script>
+
+<style>
+  #app{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light navigation">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <router-link to="/" class="nav-link routes"> Glavna </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/" class="nav-link routes"> Psi </router-link>
+                </li>
+                <li class="nav-item">
+                    <img src="https://friendlystock.com/wp-content/uploads/2018/10/6-kawaii-planet-jupiter-cartoon-clipart.jpg" alt="" id="jupiterImg">
+                </li>
+                <li class="nav-item">
+                    <router-link to="/" class="nav-link routes"> Clanstvo </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/" class="nav-link routes"> Kontakt </router-link>
+                </li>
+            </ul>
+        </nav>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+    .navigation {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        align-content: center;
+    }
+    #jupiterImg {
+        width: 150px;
+    }
+    .routes {
+        padding-top: 50px;
+        margin-left: 100px;
+        margin-right: 50px;
+        font-size: 30px;
+    }
+</style>
